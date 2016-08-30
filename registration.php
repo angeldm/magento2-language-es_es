d9bfb8f3b2e1ee80b68d6892a88ebb4beebe77a8
àngel <angeldm@gmail.com>
2016-08-30 18:06:28 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -10,6 +10,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'eusonlito_es_es',
+    'angeldm_es_es',
     __DIR__
 );
